4dad2cd3605483bafeabcfc27ffe953142a94089
Terje Sandstrom <terje@hermit.no>
2018-08-01 22:59:39 +0200

    Added test for stringextension

--- TestProj.Tests/TestExtensionMethodTests.cs
+++ TestProj.Tests/TestExtensionMethodTests.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using NUnit.Framework;
 using FluentAssertions;
 using System.Threading;
+using TestProj.Infrastructure.Extensions;
 
 namespace TestProj.Tests
 {
@@ -15,11 +16,22 @@ namespace TestProj.Tests
         [TestCase("nn-NO")]
         public void ConvertToDateTime_WithVaryingCulture_DayMonthYearFormat_ExpectSuccessfulConversion(string culture)
         {
+            int x;
             Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
 
             var result = "5/10/2018".ConvertToDateTime();
-
+            x = 4;
             result.Should().BeSameDateAs(new DateTime(2018, 10, 5));
+            //Assert.That(x,Is.EqualTo(4));
+        }
+
+
+        [Test]
+        public void TestExtensionMethod()
+        {
+            string s = "a b c";
+            var res = s.RemoveWhitespace();
+            Assert.That(res,Is.EqualTo("abc"));
         }
     }
 }
